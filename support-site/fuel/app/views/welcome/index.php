0534beb853d2e5f4869bb083e8802713969de6f6
smeghead <smeghead7@gmail.com>
2012-01-15 20:50:01 +0900

    added faq and inquiry pages.

--- support-site/fuel/app/views/welcome/index.php
+++ support-site/fuel/app/views/welcome/index.php
@@ -9,29 +9,129 @@
   <script src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
 </head>
 <body>
-<div data-role="page">
+<div data-role="page" id="home">
 
   <div data-role="header">
     <h1>
       無駄新聞 サポート
     </h1>
-  </div><!-- /header -->
-
-  <div data-role="footer">    
     <div data-role="navbar">
       <ul>
-        <li><a href="#" data-icon="home">ニュース</a></li>
-        <li><a href="#" data-icon="info">FAQ</a></li>
-        <li><a href="#" data-icon="search">問い合わせ</a></li>
+        <li><a href="#home" data-icon="home">ホーム</a></li>
+        <li><a href="#faq" data-icon="info">FAQ</a></li>
+        <li><a href="#inquiry" data-icon="search">問い合わせ</a></li>
       </ul>
     </div><!-- /navbar -->
-  </div><!-- /footer -->
+  </div><!-- /header -->
+
 
   <div data-role="content"> 
     <img src="/images/icon.png"/>
-    <p>Hello world</p>    
+    <p>
+一番カジュアルな新聞アプリ 無駄新聞です。よくわからない設定項目などは無しで、てっとり早く楽しんで下さい。
+優良ブログの新着記事をいちはやくお届けします。
+無駄新聞は、とても簡単に使える新聞アプリです。
+暇潰しや雑談のタネに使って下さい。
+
+<h4> ニュース記事提供元 </h4>
+<ul>
+<li>らばQ</li>
+<li>痛いニュース(ﾉ∀`)</li>
+<li>GIGAZINE</li>
+</ul>
+    </p>    
   </div><!-- /content -->
 
+  <div data-role="footer">    
+    <h5 style="text-align: right;">Copyright smeghead.</h5>
+  </div><!-- /footer -->
+</div><!-- /page -->
+
+<div data-role="page" id="faq">
+
+  <div data-role="header">
+    <h1>
+      無駄新聞 サポート
+    </h1>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#home" data-icon="home">ホーム</a></li>
+        <li><a href="#faq" data-icon="info">FAQ</a></li>
+        <li><a href="#inquiry" data-icon="search">問い合わせ</a></li>
+      </ul>
+    </div><!-- /navbar -->
+  </div><!-- /header -->
+
+
+  <div data-role="content"> 
+      <p>
+      無駄新聞について問題がありましたら、この解決方法をお試し下さい。
+      解決方法が無かった場合は、<a href="#inquiry" data-role="button" data-inline="true">問い合わせ</a>からご連絡下さい。
+      </p>
+      <ul data-role="listview">
+        <li data-role="list-divider">アプリの動作</li>
+        <li><a href="index.html">アプリが起動しない</a></li>
+        <li><a href="index.html">新着通知時に音がうるさい</a></li>
+        <li><a href="index.html">バッテリーを節約したい</a></li>
+        <li data-role="list-divider">その他</li>
+        <li><a href="index.html">無料ですか？</a></li>
+      </ul>
+      <p>
+      解決方法が無かった場合は、<a href="#inquiry" data-role="button" data-inline="true">問い合わせ</a>からご連絡下さい。
+      </p>
+  </div><!-- /content -->
+
+  <div data-role="footer">    
+    <h5 style="text-align: right;">Copyright smeghead.</h5>
+  </div><!-- /footer -->
+</div><!-- /page -->
+
+
+<div data-role="page" id="inquiry">
+
+  <div data-role="header">
+    <h1>
+      無駄新聞 サポート
+    </h1>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#home" data-icon="home">ホーム</a></li>
+        <li><a href="#faq" data-icon="info">FAQ</a></li>
+        <li><a href="#inquiry" data-icon="search">問い合わせ</a></li>
+      </ul>
+    </div><!-- /navbar -->
+  </div><!-- /header -->
+
+  <div data-role="content"> 
+
+    <p>
+      無駄新聞について、問題や要望・感想などありましたら、下のフォームからお知らせ下さい。
+    </p>
+    <form>
+      <ul data-role="listview" data-inset="true">
+        <li data-role="fieldcontain">
+              <label for="email">メールアドレス:</label>
+              <input type="email" name="email" id="email" value=""  />
+        </li>
+        <li data-role="fieldcontain">
+              <label for="textarea">内容:</label>
+        <textarea cols="40" rows="8" name="textarea" id="textarea"></textarea>
+        </li>
+
+        <li class="ui-body ui-body-b">
+          <fieldset class="ui-grid-a">
+              <div class="ui-block-a"><button type="submit" data-theme="d">Cancel</button></div>
+              <div class="ui-block-b"><button type="submit" data-theme="a">Submit</button></div>
+            </fieldset>
+        </li>
+        
+      </ul>
+    </form>
+  </div>
+
+  <div data-role="footer">    
+    <h5 style="text-align: right;">Copyright smeghead.</h5>
+  </div><!-- /footer -->
 </div><!-- /page -->
 
 </body>
